e99d0ac4407004169b111622706e421b3b27c90e
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-07-31 21:46:33 +0530

    minor changes

--- codechefecc/src/components/home.js
+++ codechefecc/src/components/home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home(){
     return(
-        <h1 className="text-center">HOMEPAGE CODECHEF-PESUECC</h1>
+        <>
+        <h1 className="text-center">Welcome to HOMEPAGE CODECHEF-PESUECC</h1>
+        </>
     )
 }
